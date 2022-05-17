e8c4f1d7b3f03b180e85eb84c0ddaeb93d84f6f2
DominykasBubnys <dominykasbubnys@gmail.com>
2022-05-17 12:38:24 +0300

    adding theme context & fixing toggle button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import classes from "./styles/App.module.css";
 import Header from "./components/Header";
-
+import {ThemeContext} from "./components/Context/ThemeContext";
 
 const App = () => {
 
+  const [darkTheme, setDarkTheme] = useState(false);
 
-  return <div className={classes.container}>
-    < Header />
+  const toggleTheme = useCallback(() => {
+    setDarkTheme(!darkTheme)
+  }, [darkTheme])
 
+  return <ThemeContext.Provider value={{darkTheme, toggleTheme}}>
+    <div className={darkTheme ? classes.container_dark : classes.container}>
+      < Header />
     
-  </div>;
+    </div>
+  </ThemeContext.Provider>;
 };
 
 export default App;
--- src/components/ToggleSwitch.js
+++ src/components/ToggleSwitch.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import classes from "../styles/ToggleSwitch.module.css"
+import { ThemeContext } from './Context/ThemeContext';
 
 const ToggleSwitch = () => {
 
+  const theme = useContext(ThemeContext);
 
+  const onToggleTheme = () => {
+    theme.toggleTheme()
+  }
 
   return (
-    <label className={classes.switch}>
-        <input type="checkbox" />
+    <label  className={classes.switch}>
+        <input type="checkbox" onClick={onToggleTheme}/>
         <span className={classes.slider} />
     </label>
   )
